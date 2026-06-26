3a1d7af6fa6cc49f76a0725813a29eb55911cd85
arda-msa <arda.msa.dev@gmail.com>
2026-06-26 17:36:35 +0300

    refactor(yazi): use home manager module for yazi
    - Drop sshfs.yazi and mime-ext.yazi plugins.
    - Drop flavors. TODO: Use stylix.

--- home/yazi/default.nix
+++ home/yazi/default.nix
@@ -1,25 +1,158 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 let
-  symlink = config.lib.file.mkOutOfStoreSymlink;
-  dotfiles = "${config.home.homeDirectory}/nixos/home/yazi/config";
+  separators = {
+    open = "";
+    close = "";
+  };
 in
 
 {
-  home.packages = with pkgs; [
-    exiftool
-    jq
-    p7zip
-    sshfs
-    yazi
-  ];
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = false;
 
-  xdg.configFile."yazi" = {
-    source = symlink dotfiles;
-    recursive = true;
+    settings = {
+      mgr = {
+        sort_by = "alphabetical";
+        sort_sensitive = false;
+        sort_dir_first = true;
+        linemode = "none";
+        show_hidden = true;
+        show_symlink = true;
+      };
+
+      preview = {
+        wrap = "no";
+      };
+    };
+
+    theme = {
+      status = {
+        sep_left = separators;
+        sep_right = separators;
+      };
+
+      tabs = {
+        sep_outer = separators;
+        sep_inner = separators;
+      };
+
+      indicator = {
+        padding = separators;
+      };
+    };
+
+    plugins = {
+      full-border = pkgs.yaziPlugins.full-border;
+      lazygit = pkgs.yaziPlugins.lazygit;
+      open-git-remote = pkgs.fetchFromGitHub {
+        owner = "larry-oates";
+        repo = "open-git-remote.yazi";
+        rev = "72158d607c01b63bc4eb5ac6fc0dc0691b41ce8c";
+        hash = "sha256-DGiGn5NveIGU0BikQ9U3vPL2R/qo1VA9ZfElOwl6qPk=";
+      };
+      starship = pkgs.yaziPlugins.starship;
+    };
+
+    initLua = ''
+      require("full-border"):setup()
+      require("starship"):setup()
+    '';
+
+    keymap = {
+      # `gc`, `gd`, `gf`, `gg` and `gh` binds have been added to maintain an
+      # alphabetical order in the keybind popup, even though they are defaults.
+      mgr.prepend_keymap = [
+        {
+          on = [
+            "g"
+            "c"
+          ];
+          run = "cd ~/.config";
+          desc = "Go to ~/.config";
+        }
+        {
+          on = [
+            "g"
+            "d"
+          ];
+          run = "cd ~/Downloads";
+          desc = "Go to ~/Downloads";
+        }
+        {
+          on = [
+            "g"
+            "f"
+          ];
+          run = "follow";
+          desc = "Follow hovered link";
+        }
+        {
+          on = [
+            "g"
+            "g"
+          ];
+          run = "arrow top";
+          desc = "Go to top";
+        }
+        {
+          on = [
+            "g"
+            "h"
+          ];
+          run = "cd ~";
+          desc = "Go to home";
+        }
+        {
+          on = [
+            "g"
+            "i"
+          ];
+          run = "plugin lazygit";
+          desc = "Run lazygit";
+        }
+        {
+          on = [
+            "g"
+            "n"
+          ];
+          run = "cd ~/notes";
+          desc = "Go to ~/notes";
+        }
+        {
+          on = [
+            "g"
+            "o"
+          ];
+          run = "plugin open-git-remote";
+          desc = "Open git remote in browser";
+        }
+        {
+          on = [
+            "g"
+            "p"
+          ];
+          run = "cd ~/Projects";
+          desc = "Go to ~/Projects";
+        }
+        {
+          on = [
+            "g"
+            "r"
+          ];
+          run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
+          desc = "Go to git repo root";
+        }
+        {
+          on = [
+            "g"
+            "x"
+          ];
+          run = "cd ~/nixos";
+          desc = "Go to ~/nixos";
+        }
+      ];
+    };
   };
 }
