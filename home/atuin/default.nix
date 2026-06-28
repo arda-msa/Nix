ab64b9a204c4be58fe340663be6d2b442da63ba0
arda-msa <arda.msa.dev@gmail.com>
2026-06-28 19:08:10 +0300

    refactor(atuin): use home manager module for atuin
    - With the home manager module, home.preferXdgDirectories option makes
    the default login dir of atuin xdg-friendly for me.

--- home/atuin/default.nix
+++ home/atuin/default.nix
@@ -1,21 +1,8 @@
-{
-  config,
-  pkgs,
-  ...
-}:
-
-let
-  symlink = config.lib.file.mkOutOfStoreSymlink;
-  dotfiles = "${config.home.homeDirectory}/nixos/home/atuin/config";
-in
+{ ... }:
 
 {
-  home.packages = with pkgs; [
-    atuin
-  ];
-
-  xdg.configFile."atuin" = {
-    source = symlink dotfiles;
-    recursive = true;
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = false;
   };
 }
