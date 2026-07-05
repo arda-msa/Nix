80d7ff141a5cf77dc7713f127343081b2a2dc6dc
arda-msa <arda.msa.dev@gmail.com>
2026-07-05 21:16:01 +0300

    refactor(nixos): update intel graphics config
    - Use the recommended options on the nixos wiki page.

--- system/hardware/intel.nix
+++ system/hardware/intel.nix
@@ -1,14 +1,21 @@
 { pkgs, ... }:
 
 {
-  hardware = {
-    graphics = {
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-      ];
-    };
-
-    intel-gpu-tools.enable = true;
+  # https://wiki.nixos.org/wiki/Intel_Graphics
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+    ];
   };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelParams = [ "i915.enable_guc=3" ];
+
+  hardware.intel-gpu-tools.enable = true;
 }
