6de814ebb4e603fe9a19c9807a2d286bfd32782c
arda-msa <arda.msa.dev@gmail.com>
2026-07-28 10:25:19 +0300

    feat(nixos): add extra substituters

--- hosts/karadeniz/configuration.nix
+++ hosts/karadeniz/configuration.nix
@@ -69,6 +69,15 @@
     "flakes"
   ];
 
+  nix.settings = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   nix.settings.auto-optimise-store = true;
 
   zramSwap = {
