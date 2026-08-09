71e5c98fa3d5ca898208ea5c4d11d4866726870b
arda-msa <arda.msa.dev@gmail.com>
2026-08-09 20:34:50 +0300

    feat(firefox): use nixos module to manage policies

--- modules/features/programs/firefox.nix
+++ modules/features/programs/firefox.nix
@@ -1,5 +1,49 @@
 {
   flake.modules.nixos.graphical = {
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
+
+      policies = {
+        DisableAppUpdate = true;
+        DisableFirefoxAccounts = true;
+        DisableFirefoxStudies = true;
+        DisableTelemetry = true;
+        DisablePocket = true;
+        DontCheckDefaultBrowser = true;
+        OfferToSaveLogins = false;
+
+        ExtensionSettings = {
+          # uBlock Origin
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Bitwarden
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Floccus Bookmarks Sync
+          "floccus@handmadeideas.org" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/floccus/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Auto Tab Discard
+          "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/auto-tab-discard/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # FrankerFaceZ
+          "frankerfacez@frankerfacez.com" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/frankerfacez/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      };
+    };
   };
 }
